b218696e56e958d6adcd921ac90520530830a27c
TomV <tom@viner.tv>
2014-11-11 23:36:43 +0000

    attempt to fix Django submodule

--- src/collector/models.py
+++ src/collector/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.utils import timezone
 
 class Call(models.Model):
-    submitted_at = models.DateTimeField(auto_now_add=True, default=timezone.now)
+    submitted_at = models.DateTimeFieldd(efault=timezone.now)
     # headers = hstore
     # data = hstore
