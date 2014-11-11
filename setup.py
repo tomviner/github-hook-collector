20b1c10456a1e294b31d8990ae709c3dc4a30331
TomV <tom@viner.tv>
2014-11-11 17:39:59 +0000

    everything makes sense with a src directory
    No more github_hook_collector/github_hook_collector/github_hook_collector/
    Now github_hook_collector/src/github_hook_collector

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='github-hook-collector',
     version='0.1.0-dev',
-    package_dir={'': 'github_hook_collector'},
+    package_dir={'': 'src'},
     packages=['github_hook_collector', 'collector'],
     install_requires=['django>=1.7.1'],
 )
