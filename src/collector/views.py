4fce14a6ce9a9e3d1e55a53fb8e5bc5fb6f73da7
TomV <tom@viner.tv>
2014-12-15 09:06:37 +0000

    turn off csrf for external post

--- src/collector/views.py
+++ src/collector/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
+from django.utils.decorators import method_decorator
 from django.views.generic import View
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import Call
 
 
 class HookView(View):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, *args, **kwargs):
+        return super(HookView, self).dispatch(*args, **kwargs)
 
     def post(self, request):
         headers = {
